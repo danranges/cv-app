ae7e385b326afa825231f07be46c2cdb7c637689
Dan Ranges <danranges@gmail.com>
2022-04-12 19:46:16 +0200

    Add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import "./index.css";
 
 const App = () => {
+  const [isDark, setIsDark] = useState(false);
+
   const toggleDark = () => {
     document.getElementById("root").classList.toggle("dark");
+    setIsDark(!isDark);
   };
 
   return (
-    <div className='App'>
-      <Header toggleDark={toggleDark} />
+    <div className='App h-screen bg-stone-50 dark:bg-slate-900'>
+      <Header toggleDark={toggleDark} isDark={isDark} />
     </div>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,17 @@
 import "../index.css";
 import { FiMoon, FiSun } from "react-icons/fi";
 
-const Header = ({ toggleDark }) => {
+const Header = ({ toggleDark, isDark }) => {
   return (
-    <div className='flex justify-between p-1 dark:bg-slate-900'>
-      <h1>CV Builder</h1>
+    <div className='flex flex-col items-center justify-between p-1  sm:flex-row'>
+      <h1 className='mx-1 font-black text-2xl tracking-wide text-slate-900 dark:text-stone-100'>
+        CV Builder
+      </h1>
       <div className='flex align-middle'>
         <button
           onClick={toggleDark}
-          className='flex m-1 bg-indigo-900 text-white w-7 h-7 justify-center items-center text-lg rounded dark:bg-orange-300 dark:text-stone-900'>
-          <FiMoon />
-          <FiSun />
+          className='flex m-1 bg-indigo-900 text-slate-50 w-7 h-7 justify-center items-center text-lg rounded dark:bg-orange-300 dark:text-stone-900'>
+          {!isDark ? <FiMoon /> : <FiSun />}
         </button>
         <button className='m-1 w-24 h-7 rounded bg-teal-300 dark:bg-teal-400'>Save</button>
         <button className='m-1 w-24 h-7 rounded bg-stone-200 dark:bg-slate-700 dark:text-stone-100'>
